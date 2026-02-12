79a78456ba45004444bc60707ff0652b8de6d9cb
mbarton <michael.barton@rcpch.ac.uk>
2026-02-12 13:19:26 +0000

    Copy env file

--- npda_prem/settings.py
+++ npda_prem/settings.py
@@ -13,7 +13,6 @@ import os
 
 from pathlib import Path
 
-from django.core.management.utils import get_random_secret_key
 from django.utils.translation import gettext_lazy as _
 
 from dotenv import load_dotenv
@@ -28,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
