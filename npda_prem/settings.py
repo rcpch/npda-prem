f9788c2e846531b9eb91df4d2a35079ef8e1212b
mbarton <michael.barton@rcpch.ac.uk>
2026-02-12 11:52:54 +0000

    collectstatic doesn't work without something set for secret key!

--- npda_prem/settings.py
+++ npda_prem/settings.py
@@ -13,6 +13,7 @@ import os
 
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
 from django.utils.translation import gettext_lazy as _
 
 from dotenv import load_dotenv
@@ -27,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
