b9f9077ee0be70b70f125e694315ed01e37ed4ac
Michael Barton <michael@mbarton.cc>
2026-02-14 14:49:04 +0000

    Selector

--- submissions/views.py
+++ submissions/views.py
@@ -77,6 +77,19 @@ def get_current_clinic_name_with_region(request, clinics):
                 return f"{clinic['name']} - {clinic['region']}"
 
 
+def get_clinic_json(request, clinics):
+    clinic_names_with_regions = [f"{clinic['name']} - {clinic['region']}" for clinic in clinics]
+
+    selected_clinic_name_with_region = get_current_clinic_name_with_region(request, clinics)
+    
+    clinic_json = json.dumps({
+        "clinic_names_with_regions": clinic_names_with_regions,
+        "selected_clinic_name_with_region": selected_clinic_name_with_region
+    })
+
+    return clinic_json
+
+
 def clinic_or_region(request, lang):
     """Step 1: select the region (or 'unsure')."""
     if request.method == "POST":
@@ -98,20 +111,11 @@ def clinic_or_region(request, lang):
     regions = sorted(set(clinic["region"] for clinic in clinics))
     regions_with_slugs = [(REGION_SLUG_MAP[r], r) for r in regions]
 
-    clinic_names_with_regions = [f"{clinic['name']} - {clinic['region']}" for clinic in get_all_clinics()]
-
-    selected_clinic_name_with_region = get_current_clinic_name_with_region(request, clinics)
-    
-    clinic_json = json.dumps({
-        "clinic_names_with_regions": clinic_names_with_regions,
-        "selected_clinic_name_with_region": selected_clinic_name_with_region
-    })
-
     return render(request, "submissions/clinic_or_region.html", {
         "lang": lang,
         "region": None, # always select again
         "regions_with_slugs": regions_with_slugs,
-        "clinic_json": clinic_json
+        "clinic_json": get_clinic_json(request, clinics),
     })
 
 
@@ -397,4 +401,10 @@ def start_again(request):
 
 
 def tablet_mode(request):
-    return render(request, "submissions/tablet-mode.html", {})
+    clinics = get_all_clinics()
+
+    ctx = {
+        "clinic_json": get_clinic_json(request, clinics),
+    }
+
+    return render(request, "submissions/tablet-mode.html", ctx)
